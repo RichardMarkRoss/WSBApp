48701ed0a9276ced77249eca023ea6398d2ed415
Richard <35225988+RichardMarkRoss@users.noreply.github.com>
2023-10-17 14:36:58 +0200

    npm complete

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import { View, StatusBar, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <WebComponent style={styles.container} />
-        </View>
-    );
+  const [externalUrl, setExternalUrl] = useState(null);
+
+  const handleNavigationStateChange = (navState) => {
+    if (navState.url && !navState.url.includes('worldsportsbetting')) {
+      Linking.openURL(navState.url)
+        .then(() => setExternalUrl(navState.url))
+        .catch((error) => console.error('Error opening URL: ', error));
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <WebComponent />
+    </View>
+  );
 }
 
 const WebComponent = () => {
-    return <WebView source={{ uri: 'https://m.worldsportsbetting.co.za/' }} />
+  return (
+    <WebView
+    source={{ uri: 'https://m.worldsportsbetting.co.za' }}
+    ignoreSslError={true}
+    thirdPartyCookiesEnabled={true}
+    geolocationEnabled={true}
+    allowFileAccess={true}
+    useWebKit={true}
+    onNavigationStateChange={this.handleNavigationStateChange}
+    injectedJavaScript={`
+      document.body.style.overscrollBehavior = 'none';
+    `}
+  />
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
+  container: {
+    flex: 1,
+  },
 });
